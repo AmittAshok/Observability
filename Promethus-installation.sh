a4c0ded699f950558d165d36f05beb0baaaf7fba
amittashok <amittashok@gmail.com>
2025-01-06 20:19:00 +0530

    chnages made in file

--- Promethus-installation.sh
+++ Promethus-installation.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-#
-#
-	helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-	helm repo update
 
+# Install helm
+curl -fsSL https://baltocdn.com/helm/signing.asc | sudo gpg --dearmor -o /usr/share/keyrings/helm.gpg
+sudo apt-get install apt-transport-https --yes
+echo "deb [signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
 
+# Create promethus cluster 
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+
+# install promethus 
+
+helm install monitoring prometheus-community/kube-prometheus-stack \
+-n monitoring \
+-f ./custom_kube_prometheus_stack.yml
